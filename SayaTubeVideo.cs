424a5fa4fa389cd0d83ef3f0242705a249a83cf2
Achmadfadjry <achmadfadjryadlilfaqih@gmail.com>
2025-03-24 04:32:15 +0700

    implementasi design by contract

--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace tpmod_06_103022300159
 {
-    class SayaTubeVideo
+    public class SayaTubeVideo
     {
-      
         private int id;
         private string title;
         private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            if (title == null || title.Length > 100)
+            {
+                throw new ArgumentException("Judul video tidak boleh null dan maksimal 100 karakter.");
+            }
+
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count > 10000000)
+            {
+                throw new ArgumentException("Penambahan play count maksimal 10.000.000 per panggilan");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Overflow terjadi pada penambahan play count.");
+            }
+        }
 
         public void PrintVideoDetails()
         {
@@ -19,4 +52,32 @@ namespace tpmod_06_103022300159
             Console.WriteLine("Title: " + this.title);
             Console.WriteLine("Play Count: " + this.playCount);
         }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SayaTubeVideo videos = new SayaTubeVideo("Tutorial Design By Contract - Achmad fadjry adlil faqih");
+            for (int i = 0; i < 3; i++)
+            {
+                videos.IncreasePlayCount(10000000);
+                videos.PrintVideoDetails();
+            }
+
+
+            try
+            {
+                SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Achmad fadjry adlil faqih");
+                video.IncreasePlayCount(10000001);
+                video.PrintVideoDetails();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
 }
